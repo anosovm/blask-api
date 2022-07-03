d9752a47541d251afa1a554135a6c1354794c68c
anosovm <m.anosov@napopravku.ru>
2022-07-04 01:31:05 +0300

    Initial commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function lol() {
+        return 'sadasdas';
+    }
+
+    public function kek(Request $request) {
+        $test = $request->get('project');
+
+        return 'govno';
+    }
+}
